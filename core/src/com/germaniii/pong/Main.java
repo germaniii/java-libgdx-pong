ed21675093c5f09bf5875fe8aec2018dd58a2acc
German III <germaniiifelisarta@gmail.com>
2021-05-20 14:12:43 +0800

    v0.1 Added Desktop Config

--- core/src/com/germaniii/pong/Main.java
+++ core/src/com/germaniii/pong/Main.java
@@ -1,6 +1,8 @@
 package com.germaniii.pong;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -12,12 +14,12 @@ public class Main extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
